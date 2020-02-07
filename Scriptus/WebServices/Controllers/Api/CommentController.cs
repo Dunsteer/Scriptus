f259c57d572a89cde4e2c6e72271bc3d64d38527
Stefan <stefan.pet@elfak.rs>
2020-02-07 18:01:27 +0100

    comment i post kontroleri

---
+++ Scriptus/WebServices/Controllers/Api/CommentController.cs
@@ -0,0 +1,44 @@
+﻿using BaseLogic.Abstractions;
+using BaseLogic.Services;
+using Commons.Models.Comment;
+using DbServices.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServices.Helpers;
+
+namespace WebServices.Controllers.Api
+{
+    [Route("api/comments")]
+    [ApiController]
+    [Authorize]
+    public class CommentController : BaseRESTController<Comment, CommentSearchModel>
+    {
+        private readonly CommentService _commentService;
+        private readonly IOptions<AppSettings> _appSettings;
+
+        public CommentController(BaseService<Comment> commentService, MapperService mapper, IOptions<AppSettings> appSettings, ILogger<PostController> logger) : base(commentService, mapper, logger)
+        {
+            // BaseService<AccessToken> accessTokenService, MapperService mapper, ILogger<AccessToken> logger): base(accessTokenService, mapper, logger)
+            _commentService = (CommentService)commentService;
+            _appSettings = appSettings;
+
+            _REST.GET.MapTo = typeof(CommentViewModel);
+            _REST.GET.MapToMin = typeof(CommentMinModel);
+            _REST.POST.MapTo = typeof(CommentViewModel);
+            _REST.POST.MapToMin = typeof(CommentMinModel);
+            _REST.PATCH.MapTo = typeof(CommentViewModel);
+            _REST.PATCH.MapToMin = typeof(CommentMinModel);
+            _REST.DELETE.MapTo = typeof(CommentViewModel);
+            _REST.DELETE.MapToMin = typeof(CommentMinModel);
+
+            _REST.PUT.Allowed = false;
+            _REST.DELETE.Allowed = false;
+        }
+    }
+}
---
+++ Scriptus/WebServices/Controllers/Api/PostController.cs
@@ -0,0 +1,44 @@
+﻿using BaseLogic.Abstractions;
+using BaseLogic.Services;
+using Commons.Models.Post;
+using DbServices.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServices.Helpers;
+
+namespace WebServices.Controllers.Api
+{
+    [Route("api/posts")]
+    [ApiController]
+    [Authorize]
+    public class PostController : BaseRESTController<Post, PostSearchModel>
+    {
+        private readonly PostService _postService;
+        private readonly IOptions<AppSettings> _appSettings;
+
+        public PostController(BaseService<Post> postService, MapperService mapper, IOptions<AppSettings> appSettings, ILogger<PostController> logger) : base(postService, mapper, logger)
+        {
+            // BaseService<AccessToken> accessTokenService, MapperService mapper, ILogger<AccessToken> logger): base(accessTokenService, mapper, logger)
+            _postService = (PostService)postService;
+            _appSettings = appSettings;
+
+            _REST.GET.MapTo = typeof(PostViewModel);
+            _REST.GET.MapToMin = typeof(PostMinModel);
+            _REST.POST.MapTo = typeof(PostViewModel);
+            _REST.POST.MapToMin = typeof(PostMinModel);
+            _REST.PATCH.MapTo = typeof(PostViewModel);
+            _REST.PATCH.MapToMin = typeof(PostMinModel);
+            _REST.DELETE.MapTo = typeof(PostViewModel);
+            _REST.DELETE.MapToMin = typeof(PostMinModel);
+
+            _REST.PUT.Allowed = false;
+            _REST.DELETE.Allowed = false;
+        }
+    }
+}
